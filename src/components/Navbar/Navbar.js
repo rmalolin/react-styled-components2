e36933c1076d166826827bf080f96a4f340c9243
rmalolin <rmalolin@gmail.com>
2021-08-25 01:37:03 +0300

    Мобильное меню и стили для Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,78 @@
-import React from "react";
-import { Nav, NavbarContainer, NavLogo, NavIcon } from "./NavbarElements";
+import React, { useState, useEffect } from "react";
+import { FaTimes, FaBars } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
+import { Button } from "../../globalStyles";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  NavIcon,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLink,
+  NavItemBtn,
+  NavBtnLink,
+} from "./NavbarElements";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
+
   return (
-    <Nav>
-      <NavbarContainer>
-        <NavLogo to="/">
-          <NavIcon />
-          ULTRA
-        </NavLogo>
-      </NavbarContainer>
-    </Nav>
+    <IconContext.Provider value={{ color: "#fff" }}>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">
+            <NavIcon />
+            ULTRA
+          </NavLogo>
+          <MobileIcon onClick={toggleMenu}>
+            {isOpen ? <FaTimes /> : <FaBars />}
+          </MobileIcon>
+          <NavMenu onClick={toggleMenu} isOpen={isOpen}>
+            <NavItem>
+              <NavLink to="/">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/services">Services</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/products">Products</NavLink>
+            </NavItem>
+            <NavItemBtn>
+              {button ? (
+                <NavBtnLink to="/sign-up">
+                  <Button primary>Sign Up</Button>
+                </NavBtnLink>
+              ) : (
+                <NavBtnLink to="/sign-up">
+                  <Button fontBig primary>
+                    Sign Up
+                  </Button>
+                </NavBtnLink>
+              )}
+            </NavItemBtn>
+          </NavMenu>
+        </NavbarContainer>
+      </Nav>
+    </IconContext.Provider>
   );
 };
 
--- src/globalStyles.js
+++ src/globalStyles.js
@@ -18,9 +18,31 @@ export const Container = styled.div`
   padding-left: 50px;
   padding-right: 50px;
 
-  @media screen and (max-width: 990px) {
+  @media screen and (max-width: 768px) {
     padding-left: 30px;
     padding-right: 30px;
+  }
+`;
+
+export const Button = styled.button`
+  border-radius: 4px;
+  background: ${({ primary }) => (primary ? "#4B59F7" : "#0467FB")};
+  white-space: nowrap;
+  padding: ${({ big }) => (big ? "12px 64px" : "10px 20px")};
+  color: #fff;
+  font-size: ${({ fontBig }) => (fontBig ? "20px" : "16px")};
+  outline: none;
+  border: none;
+  cursor: pointer;
+
+  &:hover {
+    transition: all 0.3 ease-out;
+    background: #fff;
+    background: ${({ primary }) => (primary ? "#0467FB" : "#4B59F7")};
+  }
+
+  @media screen and (max-width: 960px) {
+    width: 100%;
   }
 `;
 
