f3903b61a86f4b36e71b001a8875fa1f3844415c
rmalolin <rmalolin@gmail.com>
2021-08-25 12:40:48 +0300

    Добавлен многоразовый компонент InfoSection и data к нему. Домашняя страничка HomePage. Подгружены svg и jpeg картинки.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import GlobalStyles from "./globalStyles";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/HomePage/Home";
 
 function App() {
   return (
     <Router>
       <GlobalStyles />
       <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
     </Router>
   );
 }
--- src/components/InfoSection/InfoSection.js
+++ src/components/InfoSection/InfoSection.js
@@ -1,7 +1,61 @@
 import React from "react";
+import {
+  Info,
+  InfoRow,
+  InfoColumn,
+  TextWrapper,
+  TopLine,
+  HeadLine,
+  Subtitle,
+  ImgWrapper,
+  Img,
+} from "./InfoSectionElements";
+import { Link } from "react-router-dom";
+import { Button, Container } from "../../globalStyles";
 
-const InfoSection = () => {
-  return <></>;
+const InfoSection = ({
+  lightBg,
+  imgStart,
+  lightTopLine,
+  lightTextSubtitle,
+  buttonText,
+  topLine,
+  headLine,
+  lightText,
+  subtitle,
+  primary,
+  img,
+  alt,
+}) => {
+  return (
+    <>
+      <Info lightBg={lightBg}>
+        <Container>
+          <InfoRow imgStart={imgStart}>
+            <InfoColumn>
+              <TextWrapper>
+                <TopLine lightTopLine={lightTopLine}>{topLine}</TopLine>
+                <HeadLine lightText={lightText}>{headLine}</HeadLine>
+                <Subtitle lightTextSubtitle={lightTextSubtitle}>
+                  {subtitle}
+                </Subtitle>
+                <Link to="sign-up">
+                  <Button big fontBig primary={primary}>
+                    {buttonText}
+                  </Button>
+                </Link>
+              </TextWrapper>
+            </InfoColumn>
+            <InfoColumn>
+              <ImgWrapper imgStart={imgStart}>
+                <Img src={img} alt={alt} />
+              </ImgWrapper>
+            </InfoColumn>
+          </InfoRow>
+        </Container>
+      </Info>
+    </>
+  );
 };
 
 export default InfoSection;
